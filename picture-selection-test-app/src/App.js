c4ad778c82665593f680332c62723b95686e80da
Leena Bojja <leena.bojja@outlook.com>
2022-12-01 13:36:52 +0200

    new commit

--- picture-selection-test-app/src/App.js
+++ picture-selection-test-app/src/App.js
@@ -3,11 +3,13 @@ import { useState } from 'react';
 import Button from "./components/Button";
 import Sound from "./components/Sound";
 import Donennext from "./components/Donennext";
+import { exerciseGuide } from "./components/Exerciseguide";
 
 function App() {
   const [choices, setChoices] = useState([]);
   const [isDoneClicked, setIsDoneClicked] = useState(false);
- const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
 
      
       let hasNext = index < choices.length + 1;
@@ -22,31 +24,36 @@ function App() {
         }
       }
 
+      function handleMoreClick()  {
+        setShowMore(!showMore);
+      }
+      let guide = exerciseGuide[index];
+
 
   return (
     <main>
       <Button className="btn-1" onClick={() => {
         setChoices([
           ...choices,
-          <img src="no.1.jpg" alt='clicked' />
+          <img className= "aft.clic1" src="no.1.jpg" alt='clicked' />
         ]);
       }} src="1.jpg"></Button>
       <Button  className="btn-2" onClick={() => {
         setChoices([
           ...choices,
-          <img src="no.2.jpg" alt='clicked' />
+          <img className= "aft.clic2"src="no.2.jpg" alt='clicked' />
         ]);
       }} src="2.jpg"></Button>
       <Button className="btn-3" onClick={() => {
         setChoices([
           ...choices,
-          <img src="no.3.jpg" alt='clicked' />
+          <img className= "aft.clic3"src="no.3.jpg" alt='clicked' />
         ]);
       }} src="3.jpg"></Button>
       <Button className="btn-4" onClick={() => {
         setChoices([
           ...choices,
-          <img src="no.4.jpg" alt='clicked' />
+          <img className= "aft.clic4"src="no.4.jpg" alt='clicked' />
         ]);
       }} src="4.jpg"></Button>
       <ul>
@@ -54,6 +61,12 @@ function App() {
           <li>{choice}</li>
         ))}
       </ul>
+      <div>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} How to do Exercise?
+        {showMore && <p>{ guide.Steps }</p>}
+      </button>
+      </div>
       <div>
         <Sound />
       </div>
---
+++ picture-selection-test-app/src/components/Exerciseguide.jsx
@@ -0,0 +1,10 @@
+export const exerciseGuide = [{
+Steps: <p>
+  <ul>
+    <li>kid have to click on audio button to begin with.</li>
+      <li>Then kid have to click on images according to the audio sounds.</li>
+      <li>After clicking on all the images kid have to click on done and then next to go on next slide</li>
+  </ul>
+</p>
+
+}]
