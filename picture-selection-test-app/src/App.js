52647a5f4071be823bb4b53ff476984816ed7e11
Leena Bojja <leena.bojja@outlook.com>
2022-12-05 09:29:29 +0200

    change made for button as neverclick

--- picture-selection-test-app/src/App.js
+++ picture-selection-test-app/src/App.js
@@ -29,32 +29,52 @@ function App() {
       }
       let guide = exerciseGuide[index];
 
+   const [neverclickedbtn1, setNC1] = useState(true);
+   const [neverclickedbtn2, setNC2] = useState(true);
+   const [neverclickedbtn3, setNC3] = useState(true);
+   const [neverclickedbtn4, setNC4] = useState(true);
 
   return (
     <main>
       <Button className="btn-1" onClick={() => {
+                if (neverclickedbtn1)
+                {
         setChoices([
           ...choices,
           <img className= "aft-clic1" src="no.1.jpg" alt='clicked' />
         ]);
+        setNC1(false);
+      }
       }} src="1.jpg"></Button>
       <Button  className="btn-2" onClick={() => {
+                if (neverclickedbtn2)
+                {
         setChoices([
           ...choices,
           <img className= "aft-clic2"src="no.2.jpg" alt='clicked' />
         ]);
+        setNC2(false);
+      }
       }} src="2.jpg"></Button>
       <Button className="btn-3" onClick={() => {
+                if (neverclickedbtn3)
+                {
         setChoices([
           ...choices,
           <img className= "aft-clic3"src="no.3.jpg" alt='clicked' />
         ]);
+        setNC3(false);
+      }
       }} src="3.jpg"></Button>
       <Button className="btn-4" onClick={() => {
+        if (neverclickedbtn4)
+        {
         setChoices([
           ...choices,
           <img className= "aft-clic4"src="no.4.jpg" alt='clicked' />
         ]);
+        setNC4(false);
+      }
       }} src="4.jpg"></Button>
       <ul>
         {choices.map(choice => (
@@ -70,15 +90,12 @@ function App() {
       <div>
         <Sound />
       </div>
-
       <div>
         <Donennext handleNextClick={handleNextClick} handleDoneClick={handleDoneClick}
         hasNext={hasNext} hasDone={isDoneClicked}/>
       </div>
       {index}
     </main >
-
-
   );
 }
 export default App;
